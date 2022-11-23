07918cb1d5a43d56be5ef9a5a5ef2a96a55c7147
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-23 06:23:25 -0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+// Marvish Chandra
+
+public static void convertHeight(feet){
+    intoInches = feet * 12;
+    intoCentimeters = feet * 30.48;
+    intoYards = feet * 0.3333333;
+    intoMeters = feet * 0.3048;
+    intoKilometers = feet * 3280.84;
+}
